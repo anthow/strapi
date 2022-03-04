25509d63b617c82589d24b5e0b15f1c180697464
anthow <anthonyenglebert@gmail.com>
2022-03-04 12:04:11 +0100

    Update server.js

--- env/production/server.js
+++ env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-    url: env("postgres://pvxdgtpgktwrvr:559b69fdb9ae584d36f0273e89ba9c024ddcc3dd64f3338616c340f5d47d0c73@ec2-54-220-243-77.eu-west-1.compute.amazonaws.com:5432/d168bgr4n1d816"),
+    url: env("MY_HEROKU_URL"),
     proxy: true,
     app: {
       keys: env.array("APP_KEYS", ["sgsgnjhgsdf", "fjghdgdsg"]),
