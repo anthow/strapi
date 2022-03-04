3119f635148df1b16c233b622387b0b9dcf52e58
anthow <anthonyenglebert@gmail.com>
2022-03-04 12:19:25 +0100

    Update server.js

--- env/production/server.js
+++ env/production/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     url: env("MY_HEROKU_URL"),
     proxy: true,
     app: {
-      keys: env.array("APP_KEYS", ["sgsgnjhgsdf", "fjghdgdsg"]),
+      keys: env.array("APP_KEYS", ["key_one", "key_two"]),
     },
   });
   
